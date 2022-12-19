2fc465a0c2dc7474e938ab807bb2faa91d5f8b5f
Ginnyspada <gspada@usc.edu>
2022-12-19 15:37:42 -0800

    updated navbar

--- src/Menu.js
+++ src/Menu.js
@@ -3,15 +3,15 @@ import "./nav.css";
 
 export default function Menu() {
   return (
-    <nav className="navbar navbar-expand-lg  mb-2">
-      <div className="container-fluid">
-        <Link to="/" className="nav-link">
+    <nav className="navbar navbar-expand-lg mb-2 ">
+      <div className="container-fluid justify-content-center">
+        <Link to="/" className="nav-brand">
           Welcome
         </Link>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse" id="menu">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
             <li className="nav-item">
-              <Link to="/quote" className="nav-link">
+              <Link to="/quote" className="nav-link justify-content-center">
                 Quotes
               </Link>
             </li>
